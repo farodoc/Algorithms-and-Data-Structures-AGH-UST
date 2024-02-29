d0f9935791cde7601c38b598a6d652485a10bfe8
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-02-29 18:34:33 +0100

    Exam updates

--- Exams/E3/egzamin3_template/zad_B/egz3b.py
+++
@@ -1,8 +0,0 @@
-from egz3btesty import runtests
-
-def maze( L ):
-    # tu prosze wpisac wlasna implementacje
-    return 0
-
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( maze, all_tests = False )
